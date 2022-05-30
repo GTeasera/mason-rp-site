04baab6a1da02ce59d2a39bf3835b96c7e809368
GTeasera <87891550+GTeasera@users.noreply.github.com>
2022-05-31 01:06:42 +0300

    registration 0.0.2 fix3

--- css/mode-controll.js
+++ css/mode-controll.js
@@ -4,9 +4,9 @@ themeMode.onclick = function() {
     let theme = document.getElementById("theme");
 
     if(theme.getAttribute("href") == "/css/style.css"){
-        theme.href = "/css/dark-mode.css";
+        theme.href = "css/dark-mode.css";
     }
     else{
-        theme.href = "/css/style.css";
+        theme.href = "css/style.css";
     }
 }
