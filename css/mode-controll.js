1c924ef10065be2ec76adc6c6196f09516655d76
GTeasera <87891550+GTeasera@users.noreply.github.com>
2022-05-30 19:46:10 +0300

    dark-mode-test

--- css/mode-controll.js
+++ css/mode-controll.js
@@ -3,10 +3,10 @@ let switchMode = document.getElementById("switchMode");
 switchMode.onclick = function() {
     let theme = document.getElementById("themeMode");
 
-    if(theme.getAttribute("href") == "light-mode.css"){
-        theme.href = "dark-mode.css";
+    if(theme.getAttribute("href") == "/css/style.css"){
+        theme.href = "/css/dark-mode.css";
     }
     else{
-        theme.href = "light-mode.css";
+        theme.href = "/css/style.css";
     }
 }
