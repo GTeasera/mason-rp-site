3ebe44a4655f6bd75c36c8a147239f1244f89e90
GTeasera <87891550+GTeasera@users.noreply.github.com>
2022-05-30 23:53:17 +0300

    dark-mode 1.0.0
    + fixed dark mode
    - removed bootstrap

--- css/mode-controll.js
+++ css/mode-controll.js
@@ -1,7 +1,7 @@
-let switchMode = document.getElementById("switchMode");
+let switchMode = document.getElementById("themeMode");
 
-switchMode.onclick = function() {
-    let theme = document.getElementById("themeMode");
+themeMode.onclick = function() {
+    let theme = document.getElementById("theme");
 
     if(theme.getAttribute("href") == "/css/style.css"){
         theme.href = "/css/dark-mode.css";
