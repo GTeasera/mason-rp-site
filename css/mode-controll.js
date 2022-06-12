1b4af7278b275eda95dcc39e4f915fb38826abec
GTeasera <87891550+GTeasera@users.noreply.github.com>
2022-06-12 11:32:11 +0300

    Version 1.1.1 fix 2

--- css/mode-controll.js
+++ css/mode-controll.js
@@ -3,12 +3,12 @@ let switchMode = document.getElementById("themeMode");
 themeMode.onclick = function() {
     let theme = document.getElementById("theme");
 
-    if(theme.getAttribute("href") == "/css/light-mode.css"){
+    if(theme.getAttribute("href") == "./css/light-mode.css"){
     console.log(theme.getAttribute("href"))
-        theme.href = "/css/dark-mode.css";
+        theme.href = "./css/dark-mode.css";
     }
     else{
     console.log(theme.getAttribute("href"))
-        theme.href = "/css/light-mode.css";
+        theme.href = "./css/light-mode.css";
     }
 }
